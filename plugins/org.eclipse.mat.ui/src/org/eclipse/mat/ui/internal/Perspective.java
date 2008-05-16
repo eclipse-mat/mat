7a1a99d485a10a0536476e605d3073a9a7c305b7
Elena Nayashkova <elena.nayashkova@sap.com>
2008-05-16 11:20:09 +0000

    enabled "org.eclipse.jdt.ui.JavaActionSet" in Memory Analyzer Perspective

--- plugins/org.eclipse.mat.ui/src/org/eclipse/mat/ui/internal/Perspective.java
+++ plugins/org.eclipse.mat.ui/src/org/eclipse/mat/ui/internal/Perspective.java
@@ -10,12 +10,13 @@
  *******************************************************************************/
 package org.eclipse.mat.ui.internal;
 
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.mat.ui.MemoryAnalyserPlugin;
 import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 import org.eclipse.ui.IPlaceholderFolderLayout;
-
+import org.osgi.framework.Bundle;
 
 public class Perspective implements IPerspectiveFactory
 {
@@ -23,7 +24,6 @@ public class Perspective implements IPerspectiveFactory
     private static final String DETAILS_VIEW = MemoryAnalyserPlugin.PLUGIN_ID + ".views.SnapshotDetailsView";
     private static final String NOTES_VIEW = MemoryAnalyserPlugin.PLUGIN_ID + ".views.TextEditorView";
     private static final String INSPECTOR_VIEW = MemoryAnalyserPlugin.PLUGIN_ID + ".views.InspectorView";
-
     private static final String ERROR_VIEW = "org.eclipse.pde.runtime.LogView";
 
     public void createInitialLayout(IPageLayout layout)
@@ -33,20 +33,25 @@ public class Perspective implements IPerspectiveFactory
 
         IFolderLayout topLeft = layout.createFolder("topLeft", IPageLayout.LEFT, (float) 0.26, editorArea);
         topLeft.addView(INSPECTOR_VIEW);
-        //        topLeft.addView("org.eclipse.pde.ui.PluginsView");
         topLeft.addPlaceholder(DETAILS_VIEW);
 
         IPlaceholderFolderLayout bottomLeft = layout.createPlaceholderFolder("bottomLeft", IPageLayout.BOTTOM,
                         (float) 0.60, DETAILS_VIEW);
         bottomLeft.addPlaceholder(HISTORY_VIEW);
 
-        layout.addView(NOTES_VIEW, IPageLayout.BOTTOM, (float) 0.80, editorArea);
-        layout.addPlaceholder(IPageLayout.ID_PROGRESS_VIEW, IPageLayout.BOTTOM, (float) 0.26, editorArea);
-
+        IFolderLayout bottomMiddle = layout.createFolder("bottomMiddle", IPageLayout.BOTTOM,
+                        (float) 0.80, editorArea);        
+        bottomMiddle.addView(NOTES_VIEW);
+        bottomMiddle.addPlaceholder(IPageLayout.ID_PROGRESS_VIEW);
+        
         layout.addShowViewShortcut(HISTORY_VIEW);
         layout.addShowViewShortcut(DETAILS_VIEW);
         layout.addShowViewShortcut(INSPECTOR_VIEW);
         layout.addShowViewShortcut(NOTES_VIEW);
-        layout.addShowViewShortcut(ERROR_VIEW);
+        layout.addShowViewShortcut(ERROR_VIEW);        
+
+        Bundle bundle = Platform.getBundle("org.eclipse.jdt.ui");
+        if (bundle != null)
+            layout.addActionSet("org.eclipse.jdt.ui.JavaActionSet");
     }
 }
