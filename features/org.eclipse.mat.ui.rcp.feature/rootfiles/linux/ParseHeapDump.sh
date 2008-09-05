7fb06637534671f9cc14b8a1c25b4900e07d7dc9
Andreas Buchen <andreas.buchen@sap.com>
2008-09-05 15:23:18 +0000

    fixed scripts (quotes, linux)

--- features/org.eclipse.mat.ui.rcp.feature/rootfiles/linux/ParseHeapDump.sh
+++ features/org.eclipse.mat.ui.rcp.feature/rootfiles/linux/ParseHeapDump.sh
@@ -1,11 +1,10 @@
-@echo off
-REM 
-REM This script parses a heap dump.
-REM
-REM Usage: ParseHeapDump.sh <path/to/dump.hprof> [report]*
-REM 
-REM The leak report has the id org.eclipse.mat.api:suspects
-REM 
+#!/bin/sh
+#
+# This script parses a heap dump.
+#
+# Usage: ParseHeapDump.sh <path/to/dump.hprof> [report]*
+#
+# The leak report has the id org.eclipse.mat.api:suspects
+#
 
-./MemoryAnalyzer -application org.eclipse.mat.api.parse %1 %2 %3 %4 %5 %6 %7 %8 %9
-	
+./MemoryAnalyzer -consolelog -application org.eclipse.mat.api.parse "$@"
--- features/org.eclipse.mat.ui.rcp.feature/rootfiles/win32/ParseHeapDump.bat
+++ features/org.eclipse.mat.ui.rcp.feature/rootfiles/win32/ParseHeapDump.bat
@@ -10,5 +10,5 @@ REM
 set _DIRNAME=.\
 if "%OS%" == "Windows_NT" set _DIRNAME=%~dp0%
 
-%_DIRNAME%\MemoryAnalyzer.exe -consoleLog -application org.eclipse.mat.api.parse %1 %2 %3 %4 %5 %6 %7 %8 %9
+"%_DIRNAME%\MemoryAnalyzer.exe" -consoleLog -application org.eclipse.mat.api.parse %1 %2 %3 %4 %5 %6 %7 %8 %9
 	
