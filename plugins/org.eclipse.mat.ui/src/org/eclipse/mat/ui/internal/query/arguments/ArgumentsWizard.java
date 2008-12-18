c86113af3d7fdda2f0b4042078b44a575f5c7067
Andreas Buchen <andreas.buchen@sap.com>
2008-12-18 15:46:14 +0000

    fix: Resize popup help if wizard resizes (for example through pressing the help button)

--- plugins/org.eclipse.mat.ui/src/org/eclipse/mat/ui/internal/query/arguments/ArgumentsWizard.java
+++ plugins/org.eclipse.mat.ui/src/org/eclipse/mat/ui/internal/query/arguments/ArgumentsWizard.java
@@ -18,24 +18,15 @@ import org.eclipse.mat.ui.MemoryAnalyserPlugin;
 import org.eclipse.mat.ui.internal.browser.QueryContextHelp;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Rectangle;
-
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
 
 public class ArgumentsWizard extends Wizard
 {
-    @Override
-    public IDialogSettings getDialogSettings()
-    {
-        IDialogSettings workbenchDialogSettings = MemoryAnalyserPlugin.getDefault().getDialogSettings();
-        IDialogSettings result = workbenchDialogSettings.getSection(ArgumentsWizard.class.getName());
-        if (result == null)
-        {
-            result = workbenchDialogSettings.addNewSection(ArgumentsWizard.class.getName());
-        }       
-        return result;
-    }
-
     private IQueryContext context;
     private ArgumentSet argumentSet;
+    private QueryContextHelp helpPopup;
 
     public ArgumentsWizard(IQueryContext context, ArgumentSet argumentSet)
     {
@@ -45,15 +36,44 @@ public class ArgumentsWizard extends Wizard
         setWindowTitle(argumentSet.getQueryDescriptor().getName());
         setForcePreviousAndNextButtons(false);
         setDefaultPageImageDescriptor(MemoryAnalyserPlugin
-                        .getImageDescriptor(MemoryAnalyserPlugin.ISharedImages.ARGUMENTS_WIZARD));          
-   }
+                        .getImageDescriptor(MemoryAnalyserPlugin.ISharedImages.ARGUMENTS_WIZARD));
+    }
+
+    @Override
+    public void createPageControls(Composite pageContainer)
+    {
+        super.createPageControls(pageContainer);
+
+        getShell().addListener(SWT.Resize, new Listener()
+        {
+            public void handleEvent(Event event)
+            {
+                relocateHelp(false);
+            }
+        });
+    }
+
+    @Override
+    public IDialogSettings getDialogSettings()
+    {
+        IDialogSettings workbenchDialogSettings = MemoryAnalyserPlugin.getDefault().getDialogSettings();
+        IDialogSettings result = workbenchDialogSettings.getSection(ArgumentsWizard.class.getName());
+        if (result == null)
+            result = workbenchDialogSettings.addNewSection(ArgumentsWizard.class.getName());
+        return result;
+    }
 
     @Override
     public void addPages()
     {
         addPage(new ArgumentsWizardPage(context, argumentSet));
+        relocateHelp(true);
+    }
 
-         if (argumentSet.getQueryDescriptor().getHelp() != null)
+    public void relocateHelp(boolean create)
+    {
+        if (argumentSet.getQueryDescriptor().getHelp() != null && //
+                        (create || (helpPopup != null && helpPopup.getShell() != null)))
         {
             getShell().getDisplay().timerExec(100, new Runnable()
             {
@@ -61,11 +81,14 @@ public class ArgumentsWizard extends Wizard
                 {
                     if (getShell() != null && !getShell().isDisposed())
                     {
+                        if (helpPopup != null && helpPopup != null)
+                            helpPopup.close();
+
                         Rectangle myBounds = getShell().getBounds();
                         Rectangle helpBounds = new Rectangle(myBounds.x, myBounds.y + myBounds.height, myBounds.width,
                                         SWT.DEFAULT);
-                        new QueryContextHelp(getShell(), ArgumentsWizard.this.argumentSet.getQueryDescriptor(),
-                                        helpBounds).open();
+                        helpPopup = new QueryContextHelp(getShell(), argumentSet.getQueryDescriptor(), helpBounds);
+                        helpPopup.open();
                     }
                 }
             });
@@ -77,6 +100,5 @@ public class ArgumentsWizard extends Wizard
     {
         return true;
     }
-    
-   
+
 }
