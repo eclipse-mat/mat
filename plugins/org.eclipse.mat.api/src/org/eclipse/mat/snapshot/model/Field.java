bd93a48fe3a9bedc50f776351eb48d51342133c8
Andrew Johnson <andrew_johnson@uk.ibm.com>
2010-04-14 10:50:34 +0000

    299371 Update javadoc

--- plugins/org.eclipse.mat.api/src/org/eclipse/mat/snapshot/model/Field.java
+++ plugins/org.eclipse.mat.api/src/org/eclipse/mat/snapshot/model/Field.java
@@ -21,22 +21,73 @@ public final class Field extends FieldDescriptor implements Serializable
 
     protected Object value;
 
+    /**
+     * Create a representation of member variable
+     * @param name the name of the field
+     * @param type the type - see IObject.Type
+     * @param value
+     * value is one of 
+     * ObjectReference - for an object field
+     * Byte - for a byte field
+     * Short - for a short field
+     * Integer - for an int field
+     * Long - for a long field
+     * Boolean - for a boolean field
+     * Char - for a char field
+     * Float - for a float field
+     * Double - for a double field
+     * @see IObject.Type
+     */
     public Field(String name, int type, Object value)
     {
         super(name, type);
         this.value = value;
     }
 
+    /**
+     * Gets the value of the field.
+     * @return
+     * ObjectReference - for an object field
+     * Byte - for a byte field
+     * Short - for a short field
+     * Integer - for an int field
+     * Long - for a long field
+     * Boolean - for a boolean field
+     * Char - for a char field
+     * Float - for a float field
+     * Double - for a double field
+     */
     public Object getValue()
     {
         return value;
     }
 
+    /**
+     * Set the value of the field.
+     * Normally the value should not be changed.
+     * Currently this is used after deserializing static fields
+     * to change the object reference to one having a link to the current snapshot.
+     * @param object
+     * ObjectReference - for an object field
+     * Byte - for a byte field
+     * Short - for a short field
+     * Integer - for an int field
+     * Long - for a long field
+     * Boolean - for a boolean field
+     * Char - for a char field
+     * Float - for a float field
+     * Double - for a double field
+     */
     public void setValue(Object object)
     {
         value = object;
     }
 
+    /**
+     * A readable representation of the field.
+     * Do not rely on the format of the result.
+     * @return a description of this field.
+     */
     public String toString()
     {
         return type + " " + name + ": \t" + value; //$NON-NLS-1$//$NON-NLS-2$
--- plugins/org.eclipse.mat.api/src/org/eclipse/mat/snapshot/model/FieldDescriptor.java
+++ plugins/org.eclipse.mat.api/src/org/eclipse/mat/snapshot/model/FieldDescriptor.java
@@ -22,32 +22,76 @@ public class FieldDescriptor implements Serializable
     protected String name;
     protected int type;
 
+    /**
+     * Create a field for a class - just contains the field name and type,
+     * not the value
+     * @param name field name
+     * @param type field type from IObject.Type 
+     * @see IObject.Type
+     */
     public FieldDescriptor(String name, int type)
     {
         this.name = name;
         this.type = type;
     }
 
+    /**
+     * Gets the field name
+     * @return
+     */
     public String getName()
     {
         return name;
     }
 
+    /**
+     * Gets the type as a number.
+     * @return as IObject.Type
+     * @see IObject.Type
+     */
     public int getType()
     {
         return type;
     }
 
+    /**
+     * Sets the name of the field.
+     * Normally the name should not be changed.
+     * @param name the name of the field.
+     * @noreference
+     */
     public void setName(String name)
     {
         this.name = name;
     }
 
+
+    /**
+     * Sets the type of the field.
+     * Normally the type should not be changed.
+     * @param type  the type of the field as IObject.Type
+     * @see IObject.Type
+     * @noreference
+     */
     public void setType(int type)
     {
         this.type = type;
     }
 
+    /**
+     * Returns the type of the field.
+     * Used for example by the object inspector pane.
+     * @return
+     * ref
+     * byte
+     * short
+     * int
+     * long
+     * boolean
+     * char
+     * float
+     * double
+     */
     public String getVerboseSignature()
     {
         if (type == IObject.Type.OBJECT)
