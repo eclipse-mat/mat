537f5181e6a19fa1e461d4955b805ca863efb0b6
Andrew Johnson <andrew_johnson@uk.ibm.com>
2010-04-16 10:10:39 +0000

    299371 Update javadoc

---
+++ plugins/org.eclipse.mat.api/src/org/eclipse/mat/snapshot/model/package-info.java
@@ -0,0 +1,5 @@
+/*
+ * Snapshot entities, for example class, instances, class loaders, fields, GC roots.
+ * These enable information about the snapshot to be processed in a standard way.
+*/
+package org.eclipse.mat.snapshot.model;
