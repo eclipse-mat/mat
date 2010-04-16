bf0163c003ec5e294cf4d2f746e0fde5359350c8
Andrew Johnson <andrew_johnson@uk.ibm.com>
2010-04-16 11:06:09 +0000

    299371 Update javadoc

---
+++ plugins/org.eclipse.mat.api/src/org/eclipse/mat/snapshot/extension/package-info.java
@@ -0,0 +1,6 @@
+/**
+Interfaces that extend the information extracted from heap dumps like threads, name resolvers etc.
+Extensions can be added using these interfaces for application or framework specific knowledge,
+or to show extra snapshot type specific data. 
+*/
+package org.eclipse.mat.snapshot.extension;
