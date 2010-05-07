a9348db51f5fed2453166b35ebda9c6b11ac08ab
Krum Tsvetkov <krum.tsvetkov@sap.com>
2010-05-07 14:19:28 +0000

    bug 312035: Need to update copyright headers in src files copyright headers for org.eclipse.mat.ui

--- plugins/org.eclipse.mat.ui/src/org/eclipse/mat/ui/internal/package-info.java
+++ plugins/org.eclipse.mat.ui/src/org/eclipse/mat/ui/internal/package-info.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2010 SAP AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     SAP AG - initial API and implementation
+ *******************************************************************************/
 /**
  * Some internal classes for error logging, the wizard for getting started and 
  * the Memory Analyzer perspective when running in Eclipse IDE (not API).
--- plugins/org.eclipse.mat.ui/src/org/eclipse/mat/ui/internal/viewer/package-info.java
+++ plugins/org.eclipse.mat.ui/src/org/eclipse/mat/ui/internal/viewer/package-info.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2010 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 /**
  * Allows filtering of the display of tables and trees, for example excluding certain class names - user interface (not API).
  * Not intended as an Application Programming Interface (API).
