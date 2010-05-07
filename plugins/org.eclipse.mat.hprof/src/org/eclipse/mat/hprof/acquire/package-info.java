aabce3f39a6ae98999b28dd03a8017a880771af3
Krum Tsvetkov <krum.tsvetkov@sap.com>
2010-05-07 12:36:53 +0000

    bug 312035: Need to update copyright headers in src files updated headers for org.eclipse.mat.hprof

--- plugins/org.eclipse.mat.hprof/src/org/eclipse/mat/hprof/acquire/package-info.java
+++ plugins/org.eclipse.mat.hprof/src/org/eclipse/mat/hprof/acquire/package-info.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2010 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 /**
  * Generator of HPROF dumps from other running processes on the system,
  * just a acquire dumps extension, not an Application Programming Interface (API).
