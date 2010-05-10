0973fe8c413e6eaa2d10900c49bbed82c4f42a86
Krum Tsvetkov <krum.tsvetkov@sap.com>
2010-05-10 08:39:28 +0000

    bug 312035: Need to update copyright headers in src files updated copyright headers for org.eclipse.mat.api

--- plugins/org.eclipse.mat.api/src/org/eclipse/mat/internal/snapshot/package-info.java
+++ plugins/org.eclipse.mat.api/src/org/eclipse/mat/internal/snapshot/package-info.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2010 SAP AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     SAP AG - initial API and implementation
+ *******************************************************************************/
 /**
  * Collects arguments for queries.
  * This is an internal package not intended as an Application Programming Interface (API).
