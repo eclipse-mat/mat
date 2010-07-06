d031629663dc8d8e5207edafd5b6a9bbbc38cdac
Andrew Johnson <andrew_johnson@uk.ibm.com>
2010-07-06 08:55:49 +0000

    318288 Have just one script to run batch mode on 32-bit and 64-bit AIX

---
+++ features/org.eclipse.mat.ui.rcp.feature/rootfiles/aix/ParseHeapDump.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# This script parses a heap dump.
+# Adjust the path to java, version 5 or later, and the heap size as required.
+# Suitable for 64-bit and 32-bit Java, but a 64-bit Java is required
+# for larger heap sizes.
+#
+# Usage: ParseHeapDump.sh <path/to/dump.dmp.zip> [report]*
+#
+# The leak report has the id org.eclipse.mat.api:suspects
+# The top component report has the id org.eclipse.mat.api:top_components
+#
+
+java -Xmx3072M -jar "`dirname "$0"`"/plugins/org.eclipse.equinox.launcher_1*.jar -consoleLog -application org.eclipse.mat.api.parse "$@"
