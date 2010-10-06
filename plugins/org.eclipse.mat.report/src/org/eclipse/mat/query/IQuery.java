5547007474362274b52b91b61cf36f6faa04184e
Andrew Johnson <andrew_johnson@uk.ibm.com>
2010-10-06 15:57:14 +0000

    327129 Improve javadoc for key classes for extension points

--- plugins/org.eclipse.mat.report/src/org/eclipse/mat/query/IQuery.java
+++ plugins/org.eclipse.mat.report/src/org/eclipse/mat/query/IQuery.java
@@ -10,16 +10,34 @@
  *******************************************************************************/
 package org.eclipse.mat.query;
 
+import java.util.regex.Pattern;
+
+import org.eclipse.mat.query.annotations.Argument;
+import org.eclipse.mat.query.annotations.Argument.Advice;
+import org.eclipse.mat.query.results.CompositeResult;
+import org.eclipse.mat.query.results.TextResult;
+import org.eclipse.mat.report.SectionSpec;
 import org.eclipse.mat.util.IProgressListener;
 
 /**
  * Interface representing a query on the heap dump.
+ * Arguments can be injected into the query using public fields marked with the {@link Argument} annotation.
+ * Typical arguments are
+ * {@link ISnapshot}, 
+ * {@link IHeapObjectArgument},
+ * {@link IContentObject}, 
+ * {@link IContextObjectSet}, 
+ * int or int[] tagged with {@link Advice.HEAP_OBJECT}.
+ * Typical arguments to be supplied by the user of the query include {@link Pattern}, boolean flags, int.
+ * Implementations of this interface need to be
+ * registered using the <code>org.eclipse.mat.api.query</code> extension point.
  */
 public interface IQuery
 {
     /**
      * The execute method is called after all arguments have been injected into
      * the query instance.
+     * Typical results are {@link TextResult}, {@link CompositeResult}, {@link SectionSpec} etc. 
      * 
      * @param listener
      *            Monitor to report progress and check for cancellation.
