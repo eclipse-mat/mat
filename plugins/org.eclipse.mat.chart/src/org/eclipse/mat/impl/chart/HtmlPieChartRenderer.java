00e0db54df98c853367aaf14b1905a549504dc30
Krum Tsvetkov <krum.tsvetkov@sap.com>
2011-04-29 12:35:12 +0000

    341880: Legend text is truncated in chart images https://bugs.eclipse.org/bugs/show_bug.cgi?id=341880

--- plugins/org.eclipse.mat.chart/src/org/eclipse/mat/impl/chart/HtmlPieChartRenderer.java
+++ plugins/org.eclipse.mat.chart/src/org/eclipse/mat/impl/chart/HtmlPieChartRenderer.java
@@ -17,15 +17,29 @@ import java.io.Writer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.eclipse.birt.chart.computation.LegendItemHints;
+import org.eclipse.birt.chart.device.EmptyUpdateNotifier;
 import org.eclipse.birt.chart.device.IDeviceRenderer;
+import org.eclipse.birt.chart.device.IImageMapEmitter;
+import org.eclipse.birt.chart.event.StructureSource;
+import org.eclipse.birt.chart.event.StructureType;
 import org.eclipse.birt.chart.exception.ChartException;
 import org.eclipse.birt.chart.factory.GeneratedChartState;
 import org.eclipse.birt.chart.factory.Generator;
 import org.eclipse.birt.chart.factory.RunTimeContext;
 import org.eclipse.birt.chart.model.Chart;
+import org.eclipse.birt.chart.model.attribute.ActionType;
 import org.eclipse.birt.chart.model.attribute.Bounds;
+import org.eclipse.birt.chart.model.attribute.TooltipValue;
+import org.eclipse.birt.chart.model.attribute.TriggerCondition;
 import org.eclipse.birt.chart.model.attribute.impl.BoundsImpl;
 import org.eclipse.birt.chart.model.attribute.impl.ColorDefinitionImpl;
+import org.eclipse.birt.chart.model.attribute.impl.TooltipValueImpl;
+import org.eclipse.birt.chart.model.data.Action;
+import org.eclipse.birt.chart.model.data.Trigger;
+import org.eclipse.birt.chart.model.data.impl.ActionImpl;
+import org.eclipse.birt.chart.model.data.impl.TriggerImpl;
+import org.eclipse.birt.chart.render.IActionRenderer;
 import org.eclipse.birt.chart.script.IScriptClassLoader;
 import org.eclipse.birt.chart.util.PluginSettings;
 import org.eclipse.mat.query.IResult;
@@ -37,7 +51,8 @@ import org.eclipse.mat.report.Renderer;
 public class HtmlPieChartRenderer implements IOutputter
 {
 
-    public void embedd(Context context, IResult result, Writer writer) throws IOException
+    @SuppressWarnings("unchecked")
+	public void embedd(Context context, IResult result, Writer writer) throws IOException
     {
         try
         {
@@ -46,7 +61,12 @@ public class HtmlPieChartRenderer implements IOutputter
             String imageFile = "chart" + context.getId() + ".png"; //$NON-NLS-1$ //$NON-NLS-2$
 
             Chart chart = ChartBuilder.create(pie, false, ColorDefinitionImpl.WHITE(), ColorDefinitionImpl.BLACK());
-
+         
+            Trigger trigger = TriggerImpl.create(TriggerCondition.ONMOUSEOVER_LITERAL,
+            		ActionImpl.create(ActionType.SHOW_TOOLTIP_LITERAL,
+                    		TooltipValueImpl.create(200, "tooltip"))); //$NON-NLS-1$
+			chart.getLegend().getTriggers().add(trigger); 
+   			
             PluginSettings ps = PluginSettings.instance();
             IDeviceRenderer render = ps.getDevice("dv.PNG"); //$NON-NLS-1$
             render.setProperty(IDeviceRenderer.FILE_IDENTIFIER, new File(context.getOutputDirectory(), imageFile));
@@ -66,9 +86,25 @@ public class HtmlPieChartRenderer implements IOutputter
 
             Generator gr = Generator.instance();
             GeneratedChartState state = gr.build(render.getDisplayServer(), chart, bo, null, rtc, null);
+            state.getRunTimeContext().setActionRenderer(new IActionRenderer() {
+			
+				public void processAction(Action action, StructureSource source) {
+					if (ActionType.SHOW_TOOLTIP_LITERAL.equals(action.getType())) {
+						TooltipValue tooltip = (TooltipValue) action.getValue();
+						if (StructureType.LEGEND_ENTRY.equals(source.getType())) {
+							LegendItemHints item = (LegendItemHints) source.getSource();
+							tooltip.setText(item.getItemText());
+						}
+					}
+			
+				}
+			});
+            render.setProperty(IDeviceRenderer.UPDATE_NOTIFIER, new EmptyUpdateNotifier(chart, state.getChartModel()));
             gr.render(render, state);
-
-            writer.append("<img src=\"").append(imageFile).append("\" width=\"800\" height=\"350\">"); //$NON-NLS-1$ //$NON-NLS-2$
+            String imageMap = ((IImageMapEmitter) render).getImageMap();
+            String mapName = "chart" + context.getId() + "map"; //$NON-NLS-1$ //$NON-NLS-2$
+            writer.append("<map name='").append(mapName).append("'>").append(imageMap).append("</map>"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+            writer.append("<img src=\"").append(imageFile).append("\" width=\"800\" height=\"350\" usemap='#").append(mapName).append("'>"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
         }
         catch (LinkageError e)
         {
