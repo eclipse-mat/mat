bd8f55326ddc3534e445fef91db0a8f6df9795ce
Andrew Johnson <andrew_johnson@uk.ibm.com>
2012-01-25 14:02:48 +0000

    356933: integrate acquire heapdump into import wizard https://bugs.eclipse.org/bugs/show_bug.cgi?id=356933

--- plugins/org.eclipse.mat.ui/src/org/eclipse/mat/ui/internal/acquire/ProviderConfigurationWizardPage.java
+++ plugins/org.eclipse.mat.ui/src/org/eclipse/mat/ui/internal/acquire/ProviderConfigurationWizardPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 SAP AG and others.
+ * Copyright (c) 2010, 2012 SAP AG and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,8 @@
  *
  * Contributors:
  *    SAP AG - initial API and implementation
+ *    IBM Corporation - refactor for new/import wizard as WizardPage,
+ *       rename from ProviderConfigurationDialog
  *******************************************************************************/
 package org.eclipse.mat.ui.internal.acquire;
 
