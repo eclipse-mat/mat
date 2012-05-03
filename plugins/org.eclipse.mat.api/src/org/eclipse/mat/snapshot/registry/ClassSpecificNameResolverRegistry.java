3a8b9b68e31353b41f8d44ccab0ebfc50586b36b
Andrew Johnson <andrew_johnson@uk.ibm.com>
2012-05-03 10:06:10 +0000

    324967: Hide queries which are not relevant - tidy up use of @Subjects in ClassSpecificNameResolver https://bugs.eclipse.org/bugs/show_bug.cgi?id=324967

--- plugins/org.eclipse.mat.api/src/org/eclipse/mat/snapshot/registry/ClassSpecificNameResolverRegistry.java
+++ plugins/org.eclipse.mat.api/src/org/eclipse/mat/snapshot/registry/ClassSpecificNameResolverRegistry.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2009 SAP AG.
+ * Copyright (c) 2008, 2012 SAP AG and IBM Corporation.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,11 +7,11 @@
  *
  * Contributors:
  *    SAP AG - initial API and implementation
+ *    IBM Corporation - refactor to use SubjectRegistry
  *******************************************************************************/
 package org.eclipse.mat.snapshot.registry;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -22,12 +22,9 @@ import org.eclipse.mat.SnapshotException;
 import org.eclipse.mat.internal.MATPlugin;
 import org.eclipse.mat.internal.Messages;
 import org.eclipse.mat.snapshot.extension.IClassSpecificNameResolver;
-import org.eclipse.mat.snapshot.extension.Subject;
-import org.eclipse.mat.snapshot.extension.Subjects;
 import org.eclipse.mat.snapshot.model.IClass;
 import org.eclipse.mat.snapshot.model.IObject;
 import org.eclipse.mat.util.MessageUtil;
-import org.eclipse.mat.util.RegistryReader;
 
 /**
  * Registry for name resolvers which resolve the names for objects of specific
@@ -41,60 +38,23 @@ public final class ClassSpecificNameResolverRegistry
     // Singleton
     // //////////////////////////////////////////////////////////////
 
-    /** inner class because RegitryReader API not public */
-    private static class RegistryImpl extends RegistryReader<IClassSpecificNameResolver>
+    /** inner class because RegistryReader API not public */
+    private static class RegistryImpl extends SubjectRegistry<IClassSpecificNameResolver>
     {
+        // For registerResolver()
         private Map<String, IClassSpecificNameResolver> resolvers;
 
         public RegistryImpl()
         {
+            // For registerResolver()
             resolvers = new HashMap<String, IClassSpecificNameResolver>();
             init(MATPlugin.getDefault().getExtensionTracker(), MATPlugin.PLUGIN_ID + ".nameResolver"); //$NON-NLS-1$
         }
 
         @Override
-        public IClassSpecificNameResolver createDelegate(IConfigurationElement configElement)
+        protected IClassSpecificNameResolver doCreateDelegate(IConfigurationElement configElement) throws CoreException
         {
-            try
-            {
-                IClassSpecificNameResolver resolver = (IClassSpecificNameResolver) configElement
-                                .createExecutableExtension("impl"); //$NON-NLS-1$
-
-                String[] subjects = extractSubjects(resolver);
-                if (subjects != null && subjects.length > 0)
-                {
-                    for (int ii = 0; ii < subjects.length; ii++)
-                        resolvers.put(subjects[ii], resolver);
-                }
-                else
-                {
-                    String msg = MessageUtil.format(Messages.ClassSpecificNameResolverRegistry_ErrorMsg_MissingSubject,
-                                    resolver.getClass().getName());
-                    Logger.getLogger(getClass().getName()).log(Level.WARNING, msg);
-                }
-
-                return resolver;
-            }
-            catch (CoreException e)
-            {
-                String msg = MessageUtil.format(
-                                Messages.ClassSpecificNameResolverRegistry_ErrorMsg_WhileCreatingResolver,
-                                configElement.getAttribute("impl")); //$NON-NLS-1$
-                Logger.getLogger(getClass().getName()).log(Level.SEVERE, msg, e);
-                return null;
-
-            }
-        }
-
-        @Override
-        protected void removeDelegate(IClassSpecificNameResolver delegate)
-        {
-            for (Iterator<IClassSpecificNameResolver> iter = resolvers.values().iterator(); iter.hasNext();)
-            {
-                IClassSpecificNameResolver r = iter.next();
-                if (r == delegate)
-                    iter.remove();
-            }
+            return (IClassSpecificNameResolver) configElement.createExecutableExtension("impl"); //$NON-NLS-1$
         }
 
         private String doResolve(IObject object)
@@ -104,7 +64,11 @@ public final class ClassSpecificNameResolverRegistry
                 IClass clazz = object.getClazz();
                 while (clazz != null)
                 {
+                    // For registerResolver()
                     IClassSpecificNameResolver resolver = resolvers.get(clazz.getName());
+                    if (resolver != null) { return resolver.resolve(object); }
+
+                    resolver = lookup(clazz.getName());
                     if (resolver != null) { return resolver.resolve(object); }
                     clazz = clazz.getSuperClass();
                 }
@@ -126,16 +90,6 @@ public final class ClassSpecificNameResolverRegistry
                                                 object.getTechnicalName()), e);
                 return null;
             }
-        }
-
-        private String[] extractSubjects(IClassSpecificNameResolver instance)
-        {
-            Subjects subjects = instance.getClass().getAnnotation(Subjects.class);
-            if (subjects != null)
-                return subjects.value();
-
-            Subject subject = instance.getClass().getAnnotation(Subject.class);
-            return subject != null ? new String[] { subject.value() } : null;
         }
 
     }
