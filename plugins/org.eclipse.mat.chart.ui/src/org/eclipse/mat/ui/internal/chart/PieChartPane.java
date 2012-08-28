bae249a3bcb1a9a87e2fc26df8009b2f4d8fb7d4
Andrew Johnson <andrew_johnson@uk.ibm.com>
2012-08-28 11:16:49 +0000

    332853: Fix FindBugs warning

--- plugins/org.eclipse.mat.chart.ui/src/org/eclipse/mat/ui/internal/chart/PieChartPane.java
+++ plugins/org.eclipse.mat.chart.ui/src/org/eclipse/mat/ui/internal/chart/PieChartPane.java
@@ -234,6 +234,7 @@ public class PieChartPane extends AbstractEditorPane implements ISelectionProvid
                             }
                             break;
                         default:
+                            break;
                     }
                     slice = slices.get(dph.getIndex());
                 }
