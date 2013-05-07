a72961e02f88d563498214c4a6ef5df78d3de032
Andrew Johnson <andrew_johnson@uk.ibm.com>
2013-05-07 13:57:17 +0000

    380858: Allow selection of which runtime to analyze when core file contains multiple java runtimes https://bugs.eclipse.org/bugs/show_bug.cgi?id=380858

--- plugins/org.eclipse.mat.tests/src/org/eclipse/mat/tests/AllTests.java
+++ plugins/org.eclipse.mat.tests/src/org/eclipse/mat/tests/AllTests.java
@@ -34,6 +34,7 @@ import org.junit.runners.Suite.SuiteClasses;
                 org.eclipse.mat.tests.snapshot.QueryLookupTest.class, //
                 org.eclipse.mat.tests.snapshot.QueriesTest.class, //
                 org.eclipse.mat.tests.snapshot.OQLTest.class, //
+                org.eclipse.mat.tests.snapshot.MultipleSnapshots.class, //
                 org.eclipse.mat.tests.ui.snapshot.panes.textPartitioning.TestClassNameExtractor.class,
                 org.eclipse.mat.tests.ui.snapshot.panes.textPartitioning.TestOQLPartitionScanner.class })
 public class AllTests
