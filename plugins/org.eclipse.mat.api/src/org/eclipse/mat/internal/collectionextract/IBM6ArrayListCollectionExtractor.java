4e301ec538ef23f33bcdaef3d5e9063ca4285640
Andrew Johnson <andrew_johnson@uk.ibm.com>
2018-05-07 17:55:34 +0100

    [534409] Failure running collections test under Java 9 - ArrayDeque
    Wrap-around length fix
    
    Change-Id: I544d4aac11c23e0669d71b4465491f1fc50969af

--- plugins/org.eclipse.mat.api/src/org/eclipse/mat/internal/collectionextract/IBM6ArrayListCollectionExtractor.java
+++ plugins/org.eclipse.mat.api/src/org/eclipse/mat/internal/collectionextract/IBM6ArrayListCollectionExtractor.java
@@ -1,5 +1,4 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2015 IBM Corporation and others
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,6 +11,8 @@
 package org.eclipse.mat.internal.collectionextract;
 
 import org.eclipse.mat.SnapshotException;
+import org.eclipse.mat.collect.ArrayInt;
+import org.eclipse.mat.snapshot.ISnapshot;
 import org.eclipse.mat.snapshot.model.IObject;
 
 public class IBM6ArrayListCollectionExtractor extends FieldArrayCollectionExtractor
@@ -47,6 +48,47 @@ public class IBM6ArrayListCollectionExtractor extends FieldArrayCollectionExtrac
         else if (firstIndex == null || lastIndex <= 0)
             return lastIndex;
         else
-            return lastIndex - firstIndex;
+        {
+            if (lastIndex >= firstIndex)
+            {
+                return lastIndex - firstIndex;
+            }
+            else
+            {
+                // Queue wraps
+                return lastIndex - firstIndex + getCapacity(coll);
+            }
+        }
+    }
+
+    @Override
+    public int[] extractEntryIds(IObject coll) throws SnapshotException
+    {
+        ISnapshot snapshot = coll.getSnapshot();
+        long referenceArray[] = extractEntries(coll).getReferenceArray();
+        Integer size = getSize(coll);
+        if (size == null)
+        {
+            return new int[0];
+        }
+        ArrayInt arr = new ArrayInt(size);
+        if (size > 0)
+        {
+            int firstIndex = (Integer) coll.resolveValue(this.firstIndex);
+            int lastIndex = (Integer) coll.resolveValue(this.lastIndex);
+            int end = getCapacity(coll);
+            for (int i = firstIndex; i < (firstIndex <= lastIndex  ? lastIndex : end); i++)
+            {
+                if (referenceArray[i] != 0)
+                    arr.add(snapshot.mapAddressToId(referenceArray[i]));
+            }
+            for (int i = 0; i < (firstIndex <= lastIndex ? 0 : lastIndex); i++)
+            {
+                if (referenceArray[i] != 0)
+                    arr.add(snapshot.mapAddressToId(referenceArray[i]));
+            }
+        }
+        return arr.toArray();
+
     }
 }
