5b16e46c6efb9b79868fa27fe498d8c80104c2fe
Andrew Johnson <andrew_johnson@uk.ibm.com>
2018-11-18 22:46:11 +0000

    [541286] Missing shallow heap size values in comparison tables
    Allow for Bytes as well as Long
    
    Change-Id: Icbeb10856b11d83fd9c560bc4408fa891ca42089

--- plugins/org.eclipse.mat.report/src/org/eclipse/mat/query/refined/TotalsCalculator.java
+++ plugins/org.eclipse.mat.report/src/org/eclipse/mat/query/refined/TotalsCalculator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2010 SAP AG.
+ * Copyright (c) 2008, 2018 SAP AG and IBM Corporation.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *    SAP AG - initial API and implementation
+ *    Andrew Johnson/IBM Corporation - also total sizes (Bytes)
  *******************************************************************************/
 package org.eclipse.mat.query.refined;
 
@@ -93,6 +94,8 @@ import org.eclipse.mat.util.IProgressListener;
                 {
                     v = ((Number) o).doubleValue();
                 }
+                else if (o instanceof Bytes)
+                    v = ((Bytes) o).getValue();
                 else
                 {
                     try
