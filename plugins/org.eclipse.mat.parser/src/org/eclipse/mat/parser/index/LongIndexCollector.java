96e6f08b7c3331cd9398333b744564f66de415d5
Jason Koch <jkoch@netflix.com>
2019-02-27 21:10:33 -0800

    Bug 277422 - Nice if heap parsing was multi-threaded
    Allow concurrent usage of IntIndexCollector and LongIndexCollector.
    
    Change-Id: Ib9bb476025ff18b23469ec529f293ccd6978b1cb
    Signed-off-by: Jason Koch <jkoch@netflix.com>

--- plugins/org.eclipse.mat.parser/src/org/eclipse/mat/parser/index/LongIndexCollector.java
+++ plugins/org.eclipse.mat.parser/src/org/eclipse/mat/parser/index/LongIndexCollector.java
@@ -2,36 +2,53 @@ package org.eclipse.mat.parser.index;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.eclipse.mat.collect.ArrayLongCompressed;
-import org.eclipse.mat.parser.index.IndexWriter.LongIndex;
+import org.eclipse.mat.collect.HashMapIntObject;
 
-public class LongIndexCollector extends LongIndex
+public class LongIndexCollector
 {
-    int mostSignificantBit;
+    final int mostSignificantBit;
+    final int size;
+    final int pageSize = IndexWriter.PAGE_SIZE_LONG;
+    final ConcurrentHashMap<Integer, ArrayLongCompressed> pages = new ConcurrentHashMap<Integer, ArrayLongCompressed>();
 
     public LongIndexCollector(int size, int mostSignificantBit)
     {
-        super(size);
+        this.size = size;
         this.mostSignificantBit = mostSignificantBit;
     }
 
-    @Override
-    protected ArrayLongCompressed getPage(int page)
+    protected ArrayLongCompressed getOrCreatePage(int page)
     {
-        ArrayLongCompressed array = (ArrayLongCompressed) pages.get(page);
-        if (array == null)
-        {
-            int ps = page < (size / pageSize) ? pageSize : size % pageSize;
-            array = new ArrayLongCompressed(ps, 63 - mostSignificantBit, 0);
-            pages.put(page, array);
-        }
-        return array;
+        ArrayLongCompressed existing = pages.get(page);
+        if (existing != null) return existing;
+
+        int ps = page < (size / pageSize) ? pageSize : size % pageSize;
+        ArrayLongCompressed newArray = new ArrayLongCompressed(ps, 63 - mostSignificantBit, 0);
+        existing = pages.putIfAbsent(page, newArray);
+        return (existing != null) ? existing : newArray;
     }
 
     public IIndexReader.IOne2LongIndex writeTo(File indexFile) throws IOException
     {
+        HashMapIntObject<Object> output = new HashMapIntObject<Object>(pages.size());
+        for(Map.Entry<Integer, ArrayLongCompressed> entry : pages.entrySet()) {
+            output.put(entry.getKey(), entry.getValue());
+        }
         // needed to re-compress
-        return new LongIndexStreamer().writeTo(indexFile, this.size, this.pages, this.pageSize);
+        return new LongIndexStreamer().writeTo(indexFile, this.size, output, this.pageSize);
+    }
+
+    public void set(int index, long value)
+    {
+        ArrayLongCompressed array = getOrCreatePage(index / pageSize);
+        // uses bit operations internally, so we should sync against the page
+        synchronized(array)
+        {
+            array.set(index % pageSize, value);
+        }
     }
 }
