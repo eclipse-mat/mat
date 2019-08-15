9cd4ef30ec713d60eaa81fc1d83005fbcd7a3dbb
Andrew Johnson <andrew_johnson@uk.ibm.com>
2019-08-15 12:41:31 +0100

    277422: Nice if heap parsing was multi-threaded
    Performance and memory usage tests
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=277422
    
    Change-Id: I9b87350515606822f01d2cf15efd3c366127a2f6

--- plugins/org.eclipse.mat.tests/src/org/eclipse/mat/tests/regression/PerfData.java
+++ plugins/org.eclipse.mat.tests/src/org/eclipse/mat/tests/regression/PerfData.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008 SAP AG.
+ * Copyright (c) 2008,2019 SAP AG and IBM Corporation.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *    SAP AG - initial API and implementation
+ *    Andrew Johnson - Xmx and thread numbers
  *******************************************************************************/
 
 package org.eclipse.mat.tests.regression;
@@ -15,11 +16,19 @@ package org.eclipse.mat.tests.regression;
 {
     private String testName;
     private String time;
+    private String usedMem;
+    private String freeMem;
+    private String totalMem;
+    private String maxMem;
 
-    public PerfData(String testName, String time)
+    public PerfData(String testName, String time, String usedMem, String freeMem, String totalMem, String maxMem)
     {
         this.testName = testName;
         this.time = time;
+        this.usedMem = usedMem;
+        this.freeMem = freeMem;
+        this.totalMem = totalMem;
+        this.maxMem = maxMem;
     }
 
     public String getTestName()
@@ -31,4 +40,25 @@ package org.eclipse.mat.tests.regression;
     {
         return time;
     }
+
+    public String getUsedMem()
+    {
+        return usedMem;
+    }
+
+    public String getFreeMem()
+    {
+        return freeMem;
+    }
+
+    public String getTotalMem()
+    {
+        return totalMem;
+    }
+
+    public String getMaxMem()
+    {
+        return maxMem;
+    }
+
 }
--- plugins/org.eclipse.mat.tests/src/org/eclipse/mat/tests/regression/TestParseApp.java
+++ plugins/org.eclipse.mat.tests/src/org/eclipse/mat/tests/regression/TestParseApp.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008 SAP AG.
+ * Copyright (c) 2008,2019 SAP AG and IBM Corporation.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *    SAP AG - initial API and implementation
+ *    Andrew Johnson - Xmx and thread numbers
  *******************************************************************************/
 package org.eclipse.mat.tests.regression;
 
@@ -25,16 +26,18 @@ public class TestParseApp
 {
     private File snapshotFile;
     private Spec report;
+    private boolean showMem;
 
-    public TestParseApp(File snapshotFile, Spec report)
+    public TestParseApp(File snapshotFile, Spec report, boolean showMem)
     {
         this.snapshotFile = snapshotFile;
         this.report = report;
+        this.showMem = showMem;
     }
 
     public void run() throws Exception
     {
-        IProgressListener listener = new ClockedProgressListener(snapshotFile, System.out);
+        IProgressListener listener = new ClockedProgressListener(snapshotFile, System.out, showMem);
 
         ISnapshot snapshot = null;
 
@@ -62,11 +65,12 @@ public class TestParseApp
         private int count = 0;
         private String absolutePath;
         private String fileName;
+        private boolean showMem;
 
         private long timestamp = 0;
         private String lastMessage = null;
 
-        /* package */ClockedProgressListener(File snapshot, PrintStream out)
+        /* package */ClockedProgressListener(File snapshot, PrintStream out, boolean showMemUsage)
         {
             this.out = out;
             this.timestamp = System.currentTimeMillis();
@@ -77,6 +81,7 @@ public class TestParseApp
             int p = this.fileName.lastIndexOf('.');
             if (p >= 0)
                 this.fileName = this.fileName.substring(0, p);
+            this.showMem = showMemUsage;
         }
 
         public void beginTask(String name, int totalWork)
@@ -99,8 +104,20 @@ public class TestParseApp
                 if (p >= 0)
                     lastMessage = lastMessage.substring(0, p) + "DUMP" + lastMessage.substring(p + fileName.length());
 
-                out.println(String.format("Task: [%02d] %s %s ms", ++count, lastMessage, String
-                                .valueOf(now - timestamp)));
+                if (showMem)
+                {
+                    long freeMem = Runtime.getRuntime().freeMemory();
+                    long totalMem = Runtime.getRuntime().totalMemory();
+                    long maxMem = Runtime.getRuntime().maxMemory();
+                    long usedMem = totalMem - freeMem;
+                    out.println(String.format("Task: [%02d] %s %s ms used %d free %d total %d max %d", ++count, lastMessage, String
+                                    .valueOf(now - timestamp), usedMem, freeMem, totalMem, maxMem));
+                }
+                else
+                {
+                    out.println(String.format("Task: [%02d] %s %s ms", ++count, lastMessage, String
+                                    .valueOf(now - timestamp)));
+                }
             }
 
             this.lastMessage = name;
