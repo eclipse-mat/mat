7113b67b804ed1efb98b7f1e516f9190f8e6f693
Andrew Johnson <andrew_johnson@uk.ibm.com>
2019-11-04 21:26:45 +0000

    552670: Add documentation for display of bytes in KB, MB, GB
    Fix for RCP tests.
    
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=552670
    
    Change-Id: I2adf28e55237d01f336a9f681b9d797fdf39b2ef

--- plugins/org.eclipse.mat.ui.rcp.tests/src/org/eclipse/mat/ui/rcp/tests/MemoryAnalyzerProductTest.java
+++ plugins/org.eclipse.mat.ui.rcp.tests/src/org/eclipse/mat/ui/rcp/tests/MemoryAnalyzerProductTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013,2018 SAP AG and IBM Corporation.
+ * Copyright (c) 2013,2019 SAP AG and IBM Corporation.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -54,7 +54,9 @@ public class MemoryAnalyzerProductTest
     {
         bot.menu("Window").click().menu("Preferences").click();
 
-        SWTBotShell shell = bot.shell("Preferences");
+        // OpenPreferenceAction createPreferenceDialogOn has (Filtered)
+        // OpenPreferenceAction NewPreferenceDialog does not
+        SWTBotShell shell = bot.shell("Preferences (Filtered)");
         shell.activate();
 
         bot.tree().expandNode("General").select("Network Connections");
