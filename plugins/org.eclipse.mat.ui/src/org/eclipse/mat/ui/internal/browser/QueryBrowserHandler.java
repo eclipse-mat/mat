71e75d20b3b8f18989a4c6db26a8084deb28c421
Andrew Johnson <andrew_johnson@uk.ibm.com>
2020-10-27 11:45:38 +0000

    567758: Add Close to the left and right option to tabs
    Change some keys
    
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=567758
    
    Change-Id: I393d45cdb5a323c88ab92ba114b368d1a9dc5779

--- plugins/org.eclipse.mat.ui/src/org/eclipse/mat/ui/internal/browser/QueryBrowserHandler.java
+++ plugins/org.eclipse.mat.ui/src/org/eclipse/mat/ui/internal/browser/QueryBrowserHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2010 SAP AG.
+ * Copyright (c) 2008, 2020 SAP AG and IBM Corporation.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,12 +7,15 @@
  *
  * Contributors:
  *    SAP AG - initial API and implementation
+ *    Andrew Johnson (IBM Corporation) - allow parameters to initialize with a particular command
  *******************************************************************************/
 package org.eclipse.mat.ui.internal.browser;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.mat.ui.editor.MultiPaneEditor;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
@@ -36,7 +39,18 @@ public class QueryBrowserHandler extends AbstractHandler
         if (!(activeEditor instanceof MultiPaneEditor))
             return null;
 
-        new QueryBrowserPopup((MultiPaneEditor) activeEditor).open();
+        QueryBrowserPopup qbp = new QueryBrowserPopup((MultiPaneEditor) activeEditor);
+        String commandName = executionEvent.getParameter("org.eclipse.mat.ui.query.browser.QueryBrowser.commandName"); //$NON-NLS-1$
+        if (commandName != null)
+        {
+            Control c = qbp.getFocusControl();
+            if (c instanceof Text)
+            {
+                Text t = (Text)c;
+                t.setText(commandName);
+            }
+        }
+        qbp.open();
 
         return null;
     }
