a167e69ed7d551ba49df13797215a0bbbeb44c70
Andrew Johnson <andrew_johnson@uk.ibm.com>
2022-02-02 17:16:56 +0000

    Bug 567819 Add more flexibility for loading a DTFJ implementation
    Example of how the DTFJ parser could exclude DTFJ
    implementations imcompatible with a dump.
    Change-Id: I691e0d7a7c7b48ed6a6030cbf6b0805674e61b4b
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=567819

--- plugins/org.eclipse.mat.dtfj/src/org/eclipse/mat/dtfj/Messages.java
+++ plugins/org.eclipse.mat.dtfj/src/org/eclipse/mat/dtfj/Messages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009,2021 IBM Corporation.
+ * Copyright (c) 2009,2022 IBM Corporation.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -114,6 +114,7 @@ public class Messages extends NLS
     public static String DTFJIndexBuilder_IgnoringJavaStackFrame;
     public static String DTFJIndexBuilder_IgnoringManagedRuntime;
     public static String DTFJIndexBuilder_ImageAddressSpaceEqualsBroken;
+    public static String DTFJIndexBuilder_IncompatibleDTFJVersion;
     public static String DTFJIndexBuilder_InterfaceShouldNotHaveASuperclass;
     public static String DTFJIndexBuilder_InvalidArrayElement;
     public static String DTFJIndexBuilder_InvalidField;
@@ -229,7 +230,12 @@ public class Messages extends NLS
     public static String DTFJIndexBuilder_UsingDTFJRoots;
     public static String DTFJIndexBuilder_UsingProcessPointerSizeNotAddressSpacePointerSize;
     public static String DTFJPreferencePage_AllMethods;
+    public static String DTFJPreferencePage_ChooseDTFJImplementation;
+    public static String DTFJPreferencePage_ChooseDTFJImplementationMsg;
     public static String DTFJPreferencePage_Description;
+    public static String DTFJPreferencePage_IncompatibleDTFJVersionsInput;
+    public static String DTFJPreferencePage_IncompatibleDTFJVersionsLabel;
+    public static String DTFJPreferencePage_IncompatibleDTFJVersionsTitle;
     public static String DTFJPreferencePage_MethodsAsClasses;
     public static String DTFJPreferencePage_NoMethods;
     public static String DTFJPreferencePage_OnlyStackFrames;
