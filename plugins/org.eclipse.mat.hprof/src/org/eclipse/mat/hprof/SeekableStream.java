b8eb7cac581745e3951c234c3127b03dc6254500
Andrew Johnson <andrew_johnson@uk.ibm.com>
2023-01-26 15:48:49 +0000

    Bug 580917: Fix deprecated warnings
    Clean up PosStream so no need to finalize
    
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=580917
    Change-Id: Ida4d04b1729643ffff3ad6f63917f00138bfa16f

--- plugins/org.eclipse.mat.hprof/src/org/eclipse/mat/hprof/SeekableStream.java
+++ plugins/org.eclipse.mat.hprof/src/org/eclipse/mat/hprof/SeekableStream.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019,2022 IBM Corporation and others.
+ * Copyright (c) 2019,2023 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -389,7 +389,7 @@ public class SeekableStream extends InputStream implements AutoCloseable
          */
         boolean isCleared()
         {
-            return savedStream.get() == null;
+            return savedStream == null || savedStream.get() == null;
         }
 
         @Override
@@ -398,19 +398,9 @@ public class SeekableStream extends InputStream implements AutoCloseable
             if (setActive(true))
             {
                 super.close();
-            }
-        }
-
-        @Override
-        protected void finalize() throws Throwable
-        {
-            try
-            {
-                close();
-            }
-            finally
-            {
-                super.finalize();
+                savedStream.clear();
+                in = null;
+                // Streams should be closed so no need to finalize()
             }
         }
     }
